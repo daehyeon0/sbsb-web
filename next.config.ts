99ef5d7c771312fd87a4e67da1fa52f662d06735
DaeHyunJoo <dd70296@gmail.com>
2026-06-18 01:18:16 +0900

    chore: add next.js eslint config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    formats: ['image/avif', 'image/webp'],
+    formats: ["image/avif", "image/webp"],
   },
 };
 
